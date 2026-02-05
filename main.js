b704bface6e55bf8a0991d856ab7fc4e672a1eb9
fineput <yaroslavadimovit@gmail.com>
2026-02-05 22:53:39 +0200

    add clickable for all day in calendar and did code review

--- main.js
+++ main.js
@@ -1,86 +1,90 @@
 const gridDayContainer = document.querySelector('.day_list');
 const gridWeekContainer = document.querySelector('.week_list');
 const month_and_year = document.querySelector('.month_and_year');
+const message = document.querySelector('.message');
 const elDayCalendar = document.querySelector('.day_list')
-
-const prev_btn = document.querySelector('.prev_btn');
-const next_btn = document.querySelector('.next_btn');
-
+const [prev_btn, next_btn] = [document.querySelector('.prev_btn'), document.querySelector('.next_btn')];
 
 let data = new Date();
+const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
 
 function renderCalendar() {
-    elDayCalendar.innerHTML = '';
-
     let month = data.getMonth();
     let year = data.getFullYear();
 
-    let firstDay = new Date(year, month, 1).getDay() || 7;
-    let lastDay = new Date(year, month + 1, 0).getDate();
-    const months = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ];
-
     month_and_year.textContent = `${months[month]} ${year}`;
 
-    let prevDays = new Date(year, month, 0).getDate();
-    let currentDays = firstDay - 1;
+    let firstDay = new Date(year, month, 1).getDay() || 7;
+    let lastDay = new Date(year, month + 1, 0).getDate();
+    let prevLastDays = new Date(year, month, 0).getDate();
 
-    for(let i = currentDays - 1; i >= 0; i--){
-        dayNum = prevDays - i;
-        elDayCalendar.innerHTML += `<div class="one_day_not_this_mounth">${dayNum}</div>`;
+    let calendarHtml = '';
+
+    // 1. Дні минулого місяця
+    for(let i = firstDay - 1; i > 0; i--){
+        calendarHtml += `
+        <div class="one_day_prev_month" data-m="-1">
+            ${prevLastDays - i + 1}
+        </div>`;
     }
 
+    // 2. Дні поточного місяця
+    const now = new Date();
     for( let day = 1; day <= lastDay; day++){
         const isToday =
-            day === new Date().getDate() &&
-            month === new Date().getMonth() &&
-            year === new Date().getFullYear();
+            day === now.getDate() &&
+            month === now.getMonth() &&
+            year === now.getFullYear();
 
-         elDayCalendar.innerHTML += `
-            <div class="${isToday ? "today" : "one_day"}">
+        calendarHtml += `
+            <div class="${isToday ? "today" : "one_day"} day-cell" data-m="0">
                 ${day}
-            </div>
-         `;
+            </div>`;
     }
 
-    const currentCell = (firstDay - 1) + lastDay;
+    // 3. Дні наступного місяця
+    const totalCells = (firstDay - 1) + lastDay;
+    const remainingSmall = 35 - totalCells;
+    const remainingBig = 42 - totalCells;
 
-    let nextDay = 1;
-
-    if(currentCell <= 35){
-        for (let i = currentCell; i < 35; i++) {
-            elDayCalendar.innerHTML += `<div class="one_day_not_this_mounth">${nextDay}</div>`;
-            nextDay++;
-        }
-    } else {
-        for (let i = currentCell; i < 42; i++) {
-            elDayCalendar.innerHTML += `<div class="one_day_not_this_mounth">${nextDay}</div>`;
-            nextDay++;
+    const renderNextMonth = (val) => {
+        for (let i = 1; i <= val; i++){
+            calendarHtml += `
+            <div class="one_day_next_month" data-m="1">
+                ${i}
+            </div>`;
         }
     }
+    totalCells < 35 ? renderNextMonth(remainingSmall) :renderNextMonth(remainingBig);
 
+    elDayCalendar.innerHTML = calendarHtml; 
 }
 
-const arrWeek = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+elDayCalendar.addEventListener('click', (e) => {
+    const target = e.target;
+    if(!target.classList.contains('day_list')){
+        const day = target.textContent.trim();
+        const offset = parseInt(target.dataset.m);
 
-for (let i = 0; i < arrWeek.length; i++) {
-    const nameDay = document.createElement('div');
-    nameDay.classList.add('name_day_of_week');
-    nameDay.textContent = arrWeek[i];
-    gridWeekContainer.appendChild(nameDay);
-}
+        let targetMonthInx = (data.getMonth() + offset + 12) % 12;
+        message.textContent = `Ви натиснули на ${day} день ${months[targetMonthInx]} місяць.`
+    }
+})
 
-prev_btn.onclick = () => {
-    data.setMonth(data.getMonth() - 1);
-    renderCalendar()
-}
+document.querySelector('.week_list').innerHTML = 
+    ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'].map(d => `<div class="name_day_of_week">${d}</div>`).join('');
 
-next_btn.onclick = () => {
+
+const changeMonth = val => {
     data.setMonth(data.getMonth() + 1);
     renderCalendar()
-}
+};
+
+prev_btn.onclick = () => changeMonth(-1);
+next_btn.onclick = () => changeMonth(1);
 
 renderCalendar()
 
