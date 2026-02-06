ecb076353957572b550ac25d886fb3b9c372150d
fineput <yaroslavadimovit@gmail.com>
2026-02-06 18:53:10 +0200

    add notes for everyday

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+document.addEventListener('DOMContentLoaded', displayNote)
+
 const gridDayContainer = document.querySelector('.day_list');
 const gridWeekContainer = document.querySelector('.week_list');
 const month_and_year = document.querySelector('.month_and_year');
-const message = document.querySelector('.message');
+const notes_day = document.querySelector('.notes_day');
 const elDayCalendar = document.querySelector('.day_list')
+const btn = document.getElementById('addNoteBtn');
 const [prev_btn, next_btn] = [document.querySelector('.prev_btn'), document.querySelector('.next_btn')];
 
 let data = new Date();
@@ -14,8 +17,10 @@ const months = [
 function renderCalendar() {
     let month = data.getMonth();
     let year = data.getFullYear();
+    let day = data.getDate()
 
     month_and_year.textContent = `${months[month]} ${year}`;
+    notes_day.textContent = `Note ${months[month]} ${day}`;
 
     let firstDay = new Date(year, month, 1).getDay() || 7;
     let lastDay = new Date(year, month + 1, 0).getDate();
@@ -70,7 +75,9 @@ elDayCalendar.addEventListener('click', (e) => {
         const offset = parseInt(target.dataset.m);
 
         let targetMonthInx = (data.getMonth() + offset + 12) % 12;
-        message.textContent = `Ви натиснули на ${day} день ${months[targetMonthInx]} місяць.`
+        notes_day.textContent = `Note ${months[targetMonthInx]} ${day}`
+    
+        displayNote();
     }
 })
 
@@ -79,12 +86,72 @@ document.querySelector('.week_list').innerHTML =
 
 
 const changeMonth = val => {
-    data.setMonth(data.getMonth() + 1);
+    data.setMonth(data.getMonth() + val);
     renderCalendar()
 };
 
 prev_btn.onclick = () => changeMonth(-1);
 next_btn.onclick = () => changeMonth(1);
 
-renderCalendar()
+function getSelectedDataKey() {
+    const titleParst = notes_day.textContent.split(' ');
+    const day = titleParst[titleParst.length - 1];
+    const monthName = titleParst[titleParst.length - 2];
+    const monthIndex = months.indexOf(monthName);
+    const year =  data.getFullYear();
 
+    return `${year}-${monthIndex + 1}-${day}`
+}
+
+btn.addEventListener('click', () => {
+    addNotes();
+})
+
+function addNotes() {
+    const input = document.getElementById('inputText');
+    const noteText = input.value.trim();
+    const dataKey = getSelectedDataKey();
+
+    if(noteText) {
+        const allNotes = JSON.parse(localStorage.getItem('calendarNotes') || '{}');
+        
+        if(!allNotes[dataKey]) allNotes[dataKey] = [];
+
+        allNotes[dataKey].push(noteText);
+        localStorage.setItem('calendarNotes', JSON.stringify(allNotes));
+
+        input.value = '';
+        displayNote();
+    }
+}
+
+function displayNote() {
+    const list = document.getElementById('notesList');
+    const dataKey = getSelectedDataKey();
+    const allNotes = JSON.parse(localStorage.getItem('calendarNotes') || '{}');
+
+    const dayNotes = allNotes[dataKey] || [];
+
+    list.innerHTML = dayNotes.map((note, index) => `
+        <div class="note">
+            <p>${index + 1}. ${note}</p>
+            <button onclick="deleteNote(${index})"><svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17 9L11 15M11 9L17 15M2.72 12.96L7.04 18.72C7.392 19.1893 7.568 19.424 7.79105 19.5932C7.9886 19.7432 8.21232 19.855 8.45077 19.9231C8.72 20 9.01334 20 9.6 20H17.2C18.8802 20 19.7202 20 20.362 19.673C20.9265 19.3854 21.3854 18.9265 21.673 18.362C22 17.7202 22 16.8802 22 15.2V8.8C22 7.11984 22 6.27976 21.673 5.63803C21.3854 5.07354 20.9265 4.6146 20.362 4.32698C19.7202 4 18.8802 4 17.2 4H9.6C9.01334 4 8.72 4 8.45077 4.07689C8.21232 4.14499 7.9886 4.25685 7.79105 4.40675C7.568 4.576 7.392 4.81067 7.04 5.28L2.72 11.04C2.46181 11.3843 2.33271 11.5564 2.28294 11.7454C2.23902 11.9123 2.23902 12.0877 2.28294 12.2546C2.33271 12.4436 2.46181 12.6157 2.72 12.96Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>`
+    ).join('');
+}
+
+function deleteNote(index) {
+    const dataKey = getSelectedDataKey();
+    const allNotes = JSON.parse(localStorage.getItem('calendarNotes') || '{}');
+
+    if(allNotes[dataKey]){
+        allNotes[dataKey].splice(index, 1);
+        localStorage.setItem('calendarNotes', JSON.stringify(notes));
+        displayNote();
+    }
+}
+
+renderCalendar();
